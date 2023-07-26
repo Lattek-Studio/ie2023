d1b6aa77109f7af77000fea89f8c6da1e599b8aa
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-07-26 19:41:18 +0300

    read only input with my id

--- bot_main.py
+++ bot_main.py
@@ -3,13 +3,15 @@ from watchdog.events import FileSystemEventHandler
 import os
 
 #function triggered by file creation
-def funky():
-    print("funky_sussy")
+def funky(read_file_path):
+    read_input = open(read_file_path, "r")
+    print(read_input.read())
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
-        if  event.src_path.split('/')[-1][0] == 's' and event.src_path.endswith('.txt'):
-            funky()
+        inp_file_name = event.src_path.split('\\')[-1]
+        if  inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
+            funky(event.src_path)
 
 if __name__ == "__main__":
 
@@ -19,7 +21,7 @@ if __name__ == "__main__":
             break
         except ValueError:
             print("Error: Invalid number")
-            
+
 
     path_to_watch = os.path.join("simulator","game") 
     event_handler = MyHandler()
