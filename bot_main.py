a4fbd45edc29f452e9268da3d78b7a03852ba3b0
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-07-26 19:58:04 +0300

    detect tura

--- bot_main.py
+++ bot_main.py
@@ -3,15 +3,19 @@ from watchdog.events import FileSystemEventHandler
 import os
 
 #function triggered by file creation
-def funky(read_file_path):
+def funky(read_file_path,tura):
+    print(tura, "\n\n\n")
     read_input = open(read_file_path, "r")
     print(read_input.read())
+
+def send_command(move,action,action_m,upgrade,tura):
+    return
 
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         inp_file_name = event.src_path.split('\\')[-1]
         if  inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
-            funky(event.src_path)
+            funky(event.src_path,int(inp_file_name.split('.')[0].split('_')[-1]))
 
 if __name__ == "__main__":
 
