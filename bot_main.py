750b79a23cd39c330696f2f189c0a95173d34a97
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-07-27 00:43:09 +0300

    added function for drawing spirals

--- bot_main.py
+++ bot_main.py
@@ -12,17 +12,52 @@ def funky(read_file_path,tura):
     send_command("U M U",tura)
     read_input.close()
 
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        inp_file_name = event.src_path.split('\\')[-1]
+        if  inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
+            funky(event.src_path,int(inp_file_name.split('.')[0].split('_')[-1]))
+
+
+
+
+
 def send_command(actions_string,tura):
     out = open(os.path.join("simulator","game","c{}_{}.txt".format(my_id,tura)), "a")
     out.write(actions_string)
     out.close()
     return
 
-class MyHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        inp_file_name = event.src_path.split('\\')[-1]
-        if  inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
-            funky(event.src_path,int(inp_file_name.split('.')[0].split('_')[-1]))
+
+
+
+#Helper for drawing spiral paths
+def get_spiral_traj(width,spirals,x,y):
+    correction_x = x
+    correction_y = y
+    x = 0
+    y= 0
+    prx=0
+    pry=0
+
+    coord_list = []
+
+    for i in range (spirals):
+        x=0-width-abs(prx)
+        coord_list.append((x+correction_x,y+correction_y))
+        y=0+width+abs(pry)
+        coord_list.append((x+correction_x,y+correction_y))
+        x=abs(x)
+        coord_list.append((x+correction_x,y+correction_y))
+        y=0-y
+        coord_list.append((x+correction_x,y+correction_y))
+        prx=x
+        pry=y
+
+    return coord_list
+
+
+
 
 if __name__ == "__main__":
 
