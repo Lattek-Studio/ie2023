efac13198638638fa5411fc5d32de898d358ab7f
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-07-26 22:33:22 +0300

    added player

--- bot_main.py
+++ bot_main.py
@@ -2,31 +2,40 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
 
-#function triggered by file creation
-def funky(read_file_path,tura):
+from reader.player import Perseus
+
+player = Perseus()
+
+
+# function triggered by file creation
+def funky(read_file_path, tura):
     read_input = open(read_file_path, "r")
-    print(read_input.read())
+    input = read_input.read()
+    player.addReading(input)
 
     print("TURA", tura)
 
-    send_command("U M U",tura)
+    send_command("U M U", tura)
     read_input.close()
 
-def send_command(actions_string,tura):
-    out = open(os.path.join("simulator","game","c{}_{}.txt".format(my_id,tura)), "a")
+
+def send_command(actions_string, tura):
+    out = open(os.path.join("simulator", "game",
+               "c{}_{}.txt".format(my_id, tura)), "a")
     out.write(actions_string)
     out.close()
     return
 
+
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         inp_file_name = event.src_path.split('\\')[-1]
-        if  inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
-            funky(event.src_path,int(inp_file_name.split('.')[0].split('_')[-1]))
+        if inp_file_name.startswith('s') and inp_file_name.endswith('.txt') and inp_file_name[1] == str(my_id):
+            funky(event.src_path, int(
+                inp_file_name.split('.')[0].split('_')[-1]))
+
 
 if __name__ == "__main__":
-
-    
 
     while True:
         try:
@@ -35,13 +44,12 @@ if __name__ == "__main__":
         except ValueError:
             print("Error: Invalid number")
 
-
-    #wait for game to start
-    while not os.path.isfile(os.path.join("simulator","game","s{}_{}.txt".format(my_id,0))):
+    # wait for game to start
+    while not os.path.isfile(os.path.join("simulator", "game", "s{}_{}.txt".format(my_id, 0))):
         continue
-    funky(os.path.join("simulator","game","s{}_{}.txt".format(my_id,0)),0)
+    funky(os.path.join("simulator", "game", "s{}_{}.txt".format(my_id, 0)), 0)
 
-    path_to_watch = os.path.join("simulator","game") 
+    path_to_watch = os.path.join("simulator", "game")
     event_handler = MyHandler()
 
     observer = Observer()
