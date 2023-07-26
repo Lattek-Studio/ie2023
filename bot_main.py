85a5388faa94b156ff2674af8872a3dcad3acb56
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-07-26 18:51:20 +0300

    added script detecting if file amogus is created in game folder

---
+++ bot_main.py
@@ -0,0 +1,28 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+
+#function triggered by file creation
+def funky():
+    print("funky")
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if "amogus" in event.src_path:
+            funky()
+
+if __name__ == "__main__":
+    path_to_watch = os.path.join("simulator","game") 
+    event_handler = MyHandler()
+
+    observer = Observer()
+    observer.schedule(event_handler, path=path_to_watch, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
